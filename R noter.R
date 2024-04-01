86e11ef5190fa1da25b39ff7cc73e4e0a45ae4b8
Asger PC <Asger.i.rasmussen@gmail.com>
2024-04-01 21:25:16 +0200

    Added Realkreditudlån and fixed kommuner before 2006.

--- R noter.R
+++ R noter.R
@@ -135,3 +135,121 @@ Ejendomspriser |>
   left_join(Kommune_ID |> select(Kommune_2, Kom.nr), by = "Kom.nr") |> 
   View()
 
+Realkreditudlån_1993_2013 <- read_excel("Data/Realkreditudlån 1993-2013 Monthly.xlsx", 
+                                                skip = 2)
+
+Realkreditudlån_1 = Realkreditudlån_1993_2013 |> 
+  select(-...1, -...2, -...3) |> 
+  rename("Month" = '...4',
+         'Udlån' = 'Alle løbetider/uspecificeret') |> 
+  filter(!is.na(Udlån)) |> 
+  mutate(Date = seq(as.Date("1993-01-01"), as.Date("2013-10-01"), by = "1 month"),
+                  Date = yearquarter(Date)) |> 
+  select(-Month)
+
+Realkreditudlån_2013_2023 <- read_excel("Data/Realkreditudlån 2013 - 2023 Monthly.xlsx", 
+                                        skip = 2)
+  
+Realkreditudlån_2 = Realkreditudlån_2013_2023 |> 
+  select(-...1, -...2, -...3, -...4, -...5, -...6) |> 
+  rename("Month" = '...7',
+         'Udlån' = 'I alt') |> 
+  filter(Udlån != '..') |> 
+  mutate(Date = seq(as.Date("2013-10-01"), as.Date("2022-12-01"), by = "1 month"),
+         Date = yearquarter(Date),
+         Udlån = as.double(Udlån)) |> 
+  select(-Month)
+
+Realkreditudlån_2022_2024 <- read_excel("Data/Realkreditudlån 2022 - 2024 Monthly.xlsx", 
+                                                skip = 2)
+
+Realkreditudlån_3 = Realkreditudlån_2022_2024 |> 
+  select(-...1, -...2, -...3, -...4, -...5, -...6) |> 
+  rename("Month" = '...7',
+         'Udlån' = `Ejerbolig og fritidshuse`) |> 
+  filter(Udlån != '..') |> 
+  mutate(Date = seq(as.Date("2022-12-01"), as.Date("2024-02-01"), by = "1 month"),
+         Date = yearquarter(Date)) |> 
+  select(-Month)
+  
+Realkreditudlån = Realkreditudlån_1 |> 
+  bind_rows(Realkreditudlån_2) |> 
+  bind_rows(Realkreditudlån_3) |> 
+  distinct(Date) |> 
+  as_tsibble(index = "Date")
+  
+Interest_rates <- read_excel("Data/Nationalbankens rente - Udlån - Monthly National.xlsx", 
+                             skip = 2) |> 
+  select(-...1) |> 
+  rename(Rate = Månedsultimo) |>
+  filter(Rate != is.na(Rate)) |> 
+  mutate(Date = seq(as.Date("1985-10-01"), as.Date("2024-02-01"), by = "1 month"),
+         Date = yearquarter(Date)) |>
+  select(-...2) |> 
+  mutate(Rate = as.double(Rate)) |> 
+  filter(Rate != is.na(Rate)) |> 
+  group_by(Date) |> 
+  summarise(Rate = mean(Rate)) |> 
+  mutate(diff_Rate = Rate - lag(Rate)) |> 
+  as_tsibble(index = "Date")
+
+
+Area <- read_excel("Data/Area Region Yearly .xlsx", 
+                   skip = 2) |> 
+  rename(Kommune = ...1) |> 
+  filter(Kommune != is.na(Kommune)) |> 
+  mutate(across("2007":"2024", as.integer)) |> 
+  pivot_longer(cols = "2007":"2024",
+               names_to = "Year",
+               values_to = "Area") |> 
+  mutate(Year = as.integer(Year)) |> 
+  as_tsibble(key = "Kommune", index = "Year")
+
+left_join(Area, by = c("Kommune_2" = "Kommune", "Year"))
+  
+Korrespondancetabel_Kommuner <- read_excel("Data/Korrespondancetabel Kommuner.xlsx")
+
+Korrespondancetabel_Kommuner
+
+Permits_Region_1993_2006 <- read_excel("Data/Permits Region 1993-2006.xlsx", skip = 2)
+
+Permits_Region_1993_2006 = Permits_Region_1993_2006 |> 
+  select(-...1,-...2,-...3) |> 
+  rename(Kommune = ...4) |> 
+  pivot_longer(cols = "1981":"2006",
+               names_to = "Year",
+               values_to = "Permits") |> 
+  mutate(Kommune = if_else(Kommune == "Copenhagen", "København", Kommune)) |> 
+  left_join(Korrespondancetabel_Kommuner, by = c("Kommune" = "AMT_KOM_TXT")) |> 
+  filter(!is.na(NUTS_KODE)) |> 
+  select(Year, Permits, NUTS_KODE) |> 
+  mutate(Year = as.integer(Year)) |> 
+  group_by(Year, NUTS_KODE) |> 
+  mutate(Permits = sum(Permits)) |> 
+  distinct(Year, NUTS_KODE, .keep_all = TRUE) |> 
+  as_tsibble(key = "NUTS_KODE", index = "Year")
+
+Permits_Region_2006_2024 <- read_excel("Data/Permits Region 2006-2024.xlsx", 
+                                       skip = 2) 
+
+Permits_Region_2006_2024 = Permits_Region_2006_2024 |> 
+  select(-...1,-...2,-...3) |> 
+  rename(Kommune = ...4) |> 
+  pivot_longer(cols = "2006Q1":"2023Q4",
+               names_to = "Year",
+               values_to = "Permits") |> 
+  mutate(Kommune = if_else(Kommune == "Copenhagen", "København", Kommune),
+         Kommune = if_else(Kommune == "Lyngby-Taarbæk", "Lyngby-Tårbæk", Kommune)) |> 
+  left_join(Korrespondancetabel_Kommuner, by = c("Kommune" = "NUTS_TXT")) |> 
+  select(Year, Permits, NUTS_KODE) |> 
+  mutate(Year = as.factor(substr(Year, 1,4)),
+         Permits = Permits) |> 
+  group_by(Year, NUTS_KODE) |> 
+  summarise(Permits = sum(Permits, na.rm = TRUE)) |> 
+  mutate(Year = as.character(Year),
+         Year = as.integer(Year)) |> 
+  as_tsibble(key = "NUTS_KODE", index = "Year") |> 
+  filter(Year != "2006")
+
+Permits = Permits_Region_1993_2006 |> 
+  bind_rows(Permits_Region_2006_2024)
